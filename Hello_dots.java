89d8a7e2fe5bedcece62f43db19315ebb7758201
Miho_S <milfy6210choco@gmail.com>
2023-01-22 17:56:02 +0900

    初回コミット

---
+++ Hello_dots.java
@@ -0,0 +1,34 @@
+public class Hello_dots {
+	public static void main(String[] args) {
+
+		int[][] picture= {{0,0,0,1,0,0,0,0},
+				           {0,0,0,1,0,0,0,0},
+				           {0,1,1,1,0,0,0,0},
+				           {0,1,0,1,0,0,0,0},
+				           {0,1,1,1,0,0,0,0},
+				           {0,0,0,0,0,0,0,0},
+				           {0,0,0,1,1,1,0,0},
+				           {0,0,0,1,0,1,0,0},
+				           {0,0,0,1,1,1,0,0},
+				           {0,0,0,0,0,0,0,0},
+				           {0,0,0,0,0,1,0,0},
+				           {0,0,0,0,1,1,1,0},
+				           {0,0,0,0,0,1,0,0},
+				           {0,0,0,0,0,1,1,0}};
+
+
+		for(int[]img:picture) {
+			for(int dia:img) {
+				if(dia==1) {
+					System.out.print("d");
+				}else {
+					System.out.print(" ");
+
+				}
+			}
+			System.out.println(" ");
+		}
+		System.out.println("");
+	}
+
+}
